fd04dd2c872e1f55300aeeca617344c779e75678
phuslee <tranphu.thpthht@gmail.com>
2022-12-03 00:42:20 +0700

    basic single product

--- javascrip/index.js
+++ javascrip/index.js
@@ -143,4 +143,24 @@ app.config(function($routeProvider) {
    $('body,html').animate({scrollTop: 0}, duration);
    });
    });
+   function DisSelect() {
+    var x = document.getElementById("inlineRadio1");
+    x.checked = false;
+    var y = document.getElementById("inlineRadio2");
+    y.checked = false;
+    var z = document.getElementById("inlineRadio3");
+    z.checked = false;
+    if (x.checked == false) {
+        document.getElementById("Size").innerHTML = " ";
+    }
+}
 
+function Checksize() {
+    var ele = document.getElementsByName('inlineRadioOptions');
+
+    for (i = 0; i < ele.length; i++) {
+        if (ele[i].checked) {
+            document.getElementById("Size").innerHTML = ele[i].value;
+        }
+    }
+}
